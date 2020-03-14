33f0f14ff25e326e3717564b3ec8eb6186620104
eyupcimen <cmneyp@gmail.com>
2020-03-14 15:36:31 +0300

    Fizz Buzz solution

--- AlgorithmsNotes.playground/Contents.swift
+++ AlgorithmsNotes.playground/Contents.swift
@@ -1,4 +1,21 @@
 import UIKit
 
-var str = "Hello, playground"
-print(str)
+/*
+ Fizz Buzz
+ */
+
+let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+for num in number {
+    if num % 15 == 0 {
+        print("\(num) fizz buzz")
+    } else if num % 3 == 0 {
+        print("\(num) fizz")
+    } else if num % 5 == 0 {
+        print("\(num) buzz")
+    } else {
+        print(num)
+    }
+}
+
+
